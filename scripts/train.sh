9a8d8c715dd5c5d8a4b7d1cb4d7eaedeef1af51f
g-tsvetkova <gabriela@uni.minerva.edu>
2024-12-13 03:41:12 +0000

    weights initialization

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 export LLM_CONFIG='mesh-xl/mesh-xl-125m'
-export NSAMPLE_PER_GPU=2
+export NSAMPLE_PER_GPU=1
 export SAMPLE_ROUNDS=100
 export OUTPUT_DIR='./checkpoints'
 
@@ -23,6 +23,6 @@ accelerate launch main.py \
     --model mesh_xl \
     --checkpoint_dir $OUTPUT_DIR \
     --batchsize_per_gpu $NSAMPLE_PER_GPU \
-    --dataset_num_workers 0 \
+    --dataset_num_workers 1 \
     --max_epoch 100
     
